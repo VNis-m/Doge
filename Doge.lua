08c075a6f3521a160f1049322f574ca2165bcdf1
VNis-m <lephuocquang2k8@gmail.com>
2024-12-30 20:31:30 +0700

    Create Doge.lua

---
+++ Doge.lua
@@ -0,0 +1,7 @@
+
+Config = {
+    namescript = "Doge",--Tên Script
+    logoscript = "92948083831768",--ID Logo Script
+    tacgia = "DinoSukuna"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
