506515967d45ef4e1add9769396c94914597b64b
VNis-m <lephuocquang2k8@gmail.com>
2024-12-31 11:58:09 +0700

    Update Doge.lua

--- Doge.lua
+++ Doge.lua
@@ -1,7 +1,7 @@
 
 Config = {
     namescript = "Doge",--Tên Script
-    logoscript = "92948083831768",--ID Logo Script
+    logoscript = "134505692874253",--ID Logo Script
     tacgia = "DinoSukuna"-- Tên Tác Giả
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
